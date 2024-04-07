18f91b020b96323b6900082f02bd90ddc92ad63c
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-07 15:18:28 +0500

    modified work

---
+++ Tasbeeh-Counter/main.js
@@ -0,0 +1,54 @@
+// ======= FUNCTION ONE ======
+
+let head1 = document.querySelector('#number1');
+let count1 = 0;
+
+function incres() {
+    head1.innerHTML = count1++
+}
+
+function reset() {
+    head1.innerHTML = count1 = 0;
+    countElement.textContent = count1;
+}
+
+// ======= FUNCTION ONE ======
+
+
+// ======= FUNCTION TWO ======
+
+let head2 = document.querySelector('#number2');
+let count2 = 0;
+
+function incres2() {
+    head2.innerHTML = count2++
+}
+
+function reset2() {
+    head2.innerHTML = count2 = 0;
+    countElement.textContent = count2;
+}
+
+// ======= FUNCTION TWO ======
+
+
+// ======= FUNCTION TWO ======
+
+let head3 = document.querySelector('#number3');
+let count3 = 0;
+
+function incres3() {
+    head3.innerHTML = count3++
+}
+
+function reset3() {
+    head3.innerHTML = count3 = 0;
+    countElement.textContent = count3;
+}
+
+// ======= FUNCTION TWO ======
+
+
+
+
+
