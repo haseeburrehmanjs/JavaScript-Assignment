10ed94c21ec66e22776a4a3259f0b28223d24766
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-23 23:54:07 +0500

    modified work

--- Toss-Assginment/main.js
+++ Toss-Assginment/main.js
@@ -3,6 +3,9 @@ let h2 = document.querySelector('h2')
 let btn = document.querySelector('button')
 let img = document.querySelector('img')
 
+function reset(){
+    img.src="./Assets/Images/flip-coin.png"
+}
 
 function tossNow(recive){
     let x = Math.ceil(Math.random(2)*2)
@@ -11,11 +14,11 @@ function tossNow(recive){
         console.log('YOU WON THE TOSS!');
         h1.innerHTML = recive,
         h2.innerHTML =  `CONGRATULAIONS! YOU WON THE TOSS! `
-        img.src="https://blog.tcea.org/wp-content/uploads/2021/09/clip2-4.png"
+        img.src="./Assets/Images/HEAD (1).png"
     }else{
         console.log('YOU LOSS THE TOSS!');
         h1.innerHTML = recive,
         h2.innerHTML =  `YOU LOSS THE TOSS!`
-        img.src="https://flipcoin.net/assets/website/extras/images/queen.png"
+        img.src="./Assets/Images/HEAD (2).png"
     }
 }
