210179f15e1c8188b3163b713aa1084a9c8b94a0
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-28 23:38:05 +0500

    modified work

--- Stop-Watch/main.js
+++ Stop-Watch/main.js
@@ -1,5 +1,5 @@
-let secound = document.querySelector('#second');
-let mint = document.querySelector('#mint');
+let secound = document.querySelector('#secound');
+let mint = document.querySelector('#minute');
 let hour = document.querySelector('#hour');
 
 
@@ -8,13 +8,24 @@ let mints = 0;
 let hours = 0;
 let startEntervel;
 
-function startBtn() {
+function startBtn(){
     startEntervel = setInterval(function(){
         sec += 1
+        console.log(sec);
+        secound.innerHTML = sec
         if(sec === 60){
             secound.innerHTML = sec
+            sec = 0
+            mints++
+            mint.innerHTML  = mints
         }
-    },1000)
+        if (mints === 60){
+            hour.innerHTML = hours
+            mints = 0
+            hours++
+            hour.innerHTML = hours
+        }
+    }, 1000)
 }
 
 function stopBtn() {
@@ -23,7 +34,9 @@ function stopBtn() {
 }
 function resetBtn() {
     clearInterval(startEntervel)
-    sec = 0
+    sec = 0;
+    mints = 0;
+    hours = 0;
     secound.innerHTML = 0;
     mint.innerHTML = 0;
     hour.innerHTML = 0;
