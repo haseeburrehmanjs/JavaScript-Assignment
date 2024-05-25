db3a2a9f68936a48b86dea7efa733117d7d6cb0e
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-05-25 09:47:17 +0500

    50% WORK DONE

--- Ecomerse WebSite/main.js
+++ Ecomerse WebSite/main.js
@@ -1,1 +1,82 @@
+let products = [
+    {
+        ProductName: 'Ring',
+        price: 200,
+        Image: src = "./images/p1.png"
+    },
+    {
+        ProductName: 'Watch',
+        price: 300,
+        Image: src = "./images/p7.png"
+    },
+    {
+        ProductName: 'Teddy Bear',
+        price: 110,
+        Image: src = "./images/p3.png"
+    },
+    {
+        ProductName: 'Flower Bouquet',
+        price: 45,
+        Image: src = "./images/p4.png"
+    },
+    {
+        ProductName: 'Teddy Bear',
+        price: 95,
+        Image: src = "./images/p5.png"
+    },
+    {
+        ProductName: 'Flower Bouquet',
+        price: 70,
+        Image: src = "./images/p6.png"
+    },
+    {
+        ProductName: 'Watch',
+        price: 400,
+        Image: src = "./images/p7.png"
+    },
+    {
+        ProductName: 'Ring',
+        price: 200,
+        Image: src = "./images/p8.png"
+    },
+    {
+        ProductName: 'Ring',
+        price: 200,
+        Image: src = "./images/p1.png"
+    },
+]
 
+
+let card = document.querySelector('#card-box');
+
+function render(){
+    for(let i = 0; i < products.length; i++){
+        card.innerHTML +=  `
+        <div class="card-box p-3" onclick="addToCard(${i})">
+            <div class="newTag d-flex justify-content-center align-items-center"><h6>New</h6></div>
+            <div class="mt-2 d-flex justify-content-center">
+            <img src="${products[i].Image}">
+            </div>
+            <div class="price-card d-flex justify-content-between mt-5">
+            <h5>${products[i].ProductName}</h5>
+            <h5>Price: <span class="price">$${products[i].price}</span></h5>
+            </div>
+        </div>
+        `
+    }
+}
+render()
+
+let cardArry = []
+
+function addToCard(i){
+    // console.log('additems');
+    if(cardArry.includes(products[i])){
+        products[i].quantity += 1
+        console.log(cardArry);
+    }else {
+        products[i].quantity = 1
+        cardArry.push(products[i])
+        console.log(cardArry);
+    }
+}
