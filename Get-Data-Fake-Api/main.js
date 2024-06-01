008690c934b092ee5cf22badf0d236eb0e0c4d2b
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-01 11:26:17 +0500

    work

--- Get-Data-Fake-Api/main.js
+++ Get-Data-Fake-Api/main.js
@@ -3,7 +3,32 @@ const card = document.querySelector('.cardsection')
 axios('https://fakestoreapi.com/products')
     .then(responce => {
         console.log(responce.data);
-        responce.data.map(item => {
+        responce.data.map((item,index) => {
+            card.innerHTML += `
+            <div class="card p-2" style="width: 18rem; ">
+            <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Category : ${item.category}</h5>
+            <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+            <p class="card-text">Price : $${item.price}</p>
+            <a id='addBtn' class="btn btn-primary" onclick='addToCard(${item},${index})'>Add To Card</a>
+            </div>
+            </div>`
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+
+function filtrdBtn(btn){
+    axios('https://fakestoreapi.com/products')
+    .then(responce => {
+        card.innerHTML = ''
+        console.log(btn.innerHTML)
+        let filterdItem = responce.data.filter(item => item.category === btn.textContent)
+        console.log(filterdItem)
+        filterdItem.map(item => {
             card.innerHTML += `
             <div class="card p-2" style="width: 18rem; ">
             <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
@@ -19,3 +44,33 @@ axios('https://fakestoreapi.com/products')
     .catch(error => {
         console.log(error);
     })
+}   
+
+function allItems(){
+    card.innerHTML = ''
+    axios('https://fakestoreapi.com/products')
+    .then(responce => {
+        console.log(responce.data);
+        responce.data.map((item,index) => {
+            card.innerHTML += `
+            <div class="card p-2" style="width: 18rem; ">
+            <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Category : ${item.category}</h5>
+            <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+            <p class="card-text">Price : $${item.price}</p>
+            <a id='addBtn' class="btn btn-primary" onclick='addToCard(${item},${index})'>Add To Card</a>
+            </div>
+            </div>`
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+}
+
+
+function addToCard(i,index){
+    console.log(i);
+}
