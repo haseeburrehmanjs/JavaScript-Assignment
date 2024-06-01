62bccd54752db488a8488d74632bd709d6856fdd
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-01 10:19:30 +0500

    work done

---
+++ Get-Data-Fake-Api/main.js
@@ -0,0 +1,21 @@
+const card = document.querySelector('.cardsection')
+
+axios('https://fakestoreapi.com/products')
+    .then(responce => {
+        console.log(responce.data);
+        responce.data.map(item => {
+            card.innerHTML += `
+            <div class="card p-2" style="width: 18rem; ">
+            <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Category : ${item.category}</h5>
+            <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+            <p class="card-text">Price : $${item.price}</p>
+            <a id='addBtn' class="btn btn-primary">Add To Card</a>
+            </div>
+            </div>`
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
