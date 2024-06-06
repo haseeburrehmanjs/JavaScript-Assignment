0530165b9164183a30aff295bd847c3a83522946
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-06 23:04:46 +0500

    modified work

--- Get-Data-Fake-Api/main.js
+++ Get-Data-Fake-Api/main.js
@@ -1,34 +1,11 @@
 const card = document.querySelector('.cardsection')
 
-axios('https://fakestoreapi.com/products')
-    .then(responce => {
-        console.log(responce.data);
-        responce.data.map((item,index) => {
-            card.innerHTML += `
-            <div class="card p-2" style="width: 18rem; ">
-            <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Category : ${item.category}</h5>
-            <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
-            <p class="card-text">Price : $${item.price}</p>
-            <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
-            </div>
-            </div>`
-        })
-    })
-    .catch(error => {
-        console.log(error);
-    })
-
-
-function filtrdBtn(btn){
+let apiData;
+function renderProduct(){
     axios('https://fakestoreapi.com/products')
-    .then(responce => {
-        card.innerHTML = ''
-        console.log(btn.innerHTML)
-        let filterdItem = responce.data.filter(item => item.category === btn.textContent)
-        console.log(filterdItem)
-        filterdItem.map((item,index) => {
+    .then(response => {
+        // console.log(response.data);
+        apiData = response.data.map((item, index) => {
             card.innerHTML += `
             <div class="card p-2" style="width: 18rem; ">
             <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
@@ -39,39 +16,114 @@ function filtrdBtn(btn){
             <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
             </div>
             </div>`
+            return item
         })
+        // console.log(renderData);
     })
     .catch(error => {
         console.log(error);
     })
-}   
+}
+renderProduct()
+
+
+function filteredItem(btn){
+    card.innerHTML = ''
+    let data = apiData.filter(item => item.category === btn.innerHTML)
+    data.map((item, index) => {
+        card.innerHTML += `
+        <div class="card p-2" style="width: 18rem; ">
+        <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Category : ${item.category}</h5>
+        <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+        <p class="card-text">Price : $${item.price}</p>
+        <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
+        </div>
+        </div>`
+    })
+    // console.log(data);/
+}
 
 function allItems(){
+    renderProduct()
     card.innerHTML = ''
-    axios('https://fakestoreapi.com/products')
-    .then(responce => {
-        console.log(responce.data);
-        responce.data.map((item,index) => {
-            card.innerHTML += `
-            <div class="card p-2" style="width: 18rem; ">
-            <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Category : ${item.category}</h5>
-            <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
-            <p class="card-text">Price : $${item.price}</p>
-            <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
-            </div>
-            </div>`
-        })
-    })
-    .catch(error => {
-        console.log(error);
-    })
-
 }
 
 
 
-function addToCard(index){
-    console.log(index);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function filtrdBtn(btn){
+//     axios('https://fakestoreapi.com/products')
+//     .then(responce => {
+//         card.innerHTML = ''
+//         console.log(btn.innerHTML)
+//         let filterdItem = responce.data.filter(item => item.category === btn.textContent)
+//         console.log(filterdItem)
+//         filterdItem.map((item,index) => {
+//             card.innerHTML += `
+//             <div class="card p-2" style="width: 18rem; ">
+//             <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//             <h5 class="card-title">Category : ${item.category}</h5>
+//             <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+//             <p class="card-text">Price : $${item.price}</p>
+//             <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
+//             </div>
+//             </div>`
+//         })
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+// }
+
+// function allItems(){
+//     card.innerHTML = ''
+//     axios('https://fakestoreapi.com/products')
+//     .then(responce => {
+//         console.log(responce.data);
+//         responce.data.map((item,index) => {
+//             card.innerHTML += `
+//             <div class="card p-2" style="width: 18rem; ">
+//             <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//             <h5 class="card-title">Category : ${item.category}</h5>
+//             <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+//             <p class="card-text">Price : $${item.price}</p>
+//             <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
+//             </div>
+//             </div>`
+//         })
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+
+// }
+
+
--- Google-Search/main.js
+++ Google-Search/main.js
@@ -4,11 +4,15 @@ let form = document.querySelector('form');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault()
-    let url = `https://www.google.com/search?q= ${search.value}`
-    // window.open(url) 
-    window.location = url
-    search.value = ''
+    if (search.value === '') {
+        // alert('Please Enter Something')
+        window.location = url
+    } else {
+        let url = `https://www.google.com/search?q= ${search.value}`
+        // window.open(url) 
+        window.location = url
+        search.value = ''
+    }
 })
-
 
 
